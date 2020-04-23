397567b6b0bb315777106fdaa99f939ab53184a0
dqxy <yanofsky.david@gmail.com>
2020-04-23 13:33:02 -0400

    JS

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -1,22 +1,27 @@
 module.exports = {
   succeed,
   fail,
-  repair,
-  get,
+  repair
 };
 
 function succeed(item) {
-  return { ...item };
+  return {
+    ...item,
+    enhancement: Math.min(item.enhancement + 1, 20)
+  };
 }
 
 function fail(item) {
-  return { ...item };
+  return {
+    ...item,
+    durability: item.durability - (item.enhancement < 15 ? 5 : 10),
+    enhancement: item.enhancement - (item.enhancement > 16 ? 1 : 0)
+  };
 }
 
 function repair(item) {
-  return { ...item };
-}
-
-function get(item) {
-  return { ...item };
+  return {
+    ...item,
+    durability: 100
+  };
 }
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,96 @@
-const enhancer = require('./enhancer.js');
+const enhancer = require("./enhancer.js");
 // test away!
+describe("enhancer.js", function () {
+  it("run all tests", () => {
+    expect(true).toBe(true);
+  });
+  describe(".succeed(item)", () => {
+    it("success return item with enhancement", () => {
+      expect(
+        enhancer.succeed({
+          name: "jon",
+          durability: 55,
+          enhancement: 9,
+        })
+      ).toEqual({
+        name: "jon",
+        durability: 55,
+        enhancement: 10,
+      });
+      expect(
+        enhancer.succeed({
+          name: "jorje",
+          durability: 76,
+          enhancement: 20,
+        })
+      ).toEqual({
+        name: "jorje",
+        durability: 76,
+        enhancement: 20,
+      });
+    });
+  });
+  describe(".fail(item)", () => {
+    it("fail return item with correct enhancement", () => {
+      expect(
+        enhancer.fail({
+          name: "ajax",
+          durability: 55,
+          enhancement: 5,
+        })
+      ).toEqual({
+        name: "ajax",
+        durability: 50,
+        enhancement: 5,
+      });
+      expect(
+        enhancer.fail({
+          name: "sally",
+          durability: 57,
+          enhancement: 15,
+        })
+      ).toEqual({
+        name: "sally",
+        durability: 47,
+        enhancement: 15,
+      });
+      expect(
+        enhancer.fail({
+          name: "syntax",
+          durability: 90,
+          enhancement: 18,
+        })
+      ).toEqual({
+        name: "syntax",
+        durability: 80,
+        enhancement: 17,
+      });
+    });
+  });
+  describe(".repair(item)", () => {
+    it("repair return item with durability", () => {
+      expect(
+        enhancer.repair({
+          name: "joe",
+          durability: 36,
+          enhancement: 0,
+        })
+      ).toEqual({
+        name: "joe",
+        durability: 100,
+        enhancement: 0,
+      });
+      expect(
+        enhancer.repair({
+          name: "joe",
+          durability: 100,
+          enhancement: 14,
+        })
+      ).toEqual({
+        name: "joe",
+        durability: 100,
+        enhancement: 14,
+      });
+    });
+});
+});
